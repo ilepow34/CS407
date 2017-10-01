716dba3df478501363aad26b673b40bab89debeb
woot <rhayex.2@gmail.com>
2017-10-01 15:02:41 -0400

    Here's the rest.

---
+++ Assets/Assets/Assets/My scripts/Unit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+	
+	Vector3[] path;
+	int targetIndex;
+	
+	public void NavMeshMoveUnit (Transform target)
+	{
+		UnityEngine.AI.NavMeshAgent navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		navMeshAgent.SetDestination(target.position);
+	}
+}

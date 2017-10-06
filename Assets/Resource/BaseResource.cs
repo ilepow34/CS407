f75f52da13058f595fe2833ed32b0e56de6b1426
rhayex <rhayex.2@gmail.com>
2017-10-06 17:06:41 -0400

    Added some comments to make it easier to understand why I created some functions and what they're to be used for.

--- Assets/Resource/BaseResource.cs
+++ Assets/Resource/BaseResource.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BaseResource : MonoBehaviour {
 
     //public variables to be used in unity scene editor (should show up automatically)
-    public float capacity;
+    public float capacity = 1000;
     // this is to store how much of a resource an object can hold.
     // another way we can do this is by making it so that the amount of resources
     // an object can hold is infinite. We could then restrict the amount of units that can
@@ -13,6 +13,9 @@ public class BaseResource : MonoBehaviour {
 
     // if we have a certain # capacity tho, we could destroy the object once resourceLeft = 0
     // or we could have it gradually refill back to full.
+    public float amount = 1;
+    //delete the above line when we make units that can take different amounts of resources.
+    //this is for bug testing purposes alone.
 
     protected float resourceLeft;
     //protected ResourceType resourceType;
@@ -29,7 +32,9 @@ public class BaseResource : MonoBehaviour {
       //  base.Start();
         resourceLeft = capacity;
     }
-	
+
+
+
     public void Remove(float amount)
     {
         resourceLeft -= amount;
@@ -38,6 +43,11 @@ public class BaseResource : MonoBehaviour {
             resourceLeft = 0;
         }
     }
+
+    //this returns true or false depending on if the resourceLeft float is at 0 or below.
+    //You would call this *somewhere* (probably in resourceUnit) prior to trying to take any
+    //resources from the object this is attached to. If it returns true, don't try to take anything.
+    //else, go for it.
     public bool isEmpty()
     {
         return resourceLeft <= 0;

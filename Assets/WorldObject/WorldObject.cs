a254f31605477431aabd234a3c9bf8a61fc4cec8
rhayex <rhayex.2@gmail.com>
2017-10-06 15:21:43 -0400

    Testing some stuff for later, reverting back for now to work on resources.

---
+++ Assets/WorldObject/WorldObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldObject : MonoBehaviour {
+
+    public string objectName;
+    public Texture2D buildImage;
+    public int cost, sellValue, hitPOints, maxHitPoints;
+
+
+    protected PlayerPrefs player;
+    protected string[] actions = { };
+    protected bool currentlySelected = false;
+	// Use this for initialization
+
+    protected virtual void Awake()
+    {
+
+    }
+	protected virtual void Start () {
+     //   player = transform.root.GetComponentInChildren<Player>();
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		
+	}
+    protected virtual void OnGui()
+    {
+
+    }
+}

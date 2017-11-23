ef6d1f2b7ab94bc3dde30cfb35db8f0e09b31565
woot <rhayex.2@gmail.com>
2017-11-22 21:25:20 -0500

    Healthbar is implemented. Still needs implementing on some prefabs. I have some ideas about how we should recode health going forward due to the way the healthbar is implemented.

---
+++ Assets/GUI stuff/SoldierHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierHealth : MonoBehaviour {
+
+	public float max_health = 100f;
+	public float cur_health = 0f;
+	public GameObject HealthBar;
+	// Use this for initialization
+	void Start () {
+		cur_health = max_health;
+		InvokeRepeating("decreasehealth", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void decreasehealth(){
+		cur_health -= 2;
+		float calc_health = cur_health / max_health;
+		setHealthbar(calc_health);
+	}
+	
+	public void setHealthbar(float myHealth){
+		HealthBar.transform.localScale = new Vector3 (myHealth, HealthBar.transform.localScale.y, HealthBar.transform.localScale.z);
+	}
+}

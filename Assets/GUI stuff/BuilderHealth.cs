7146720fbd3ff820f9797eef8005d5bdb9b12491
rhayex <rhayex.2@gmail.com>
2017-11-29 17:30:16 -0500

    units are now destroyed when they reach 0 health. Also added an attack value to the soldiers health script, doesn't do anything yet but we can implement that this week.

--- Assets/GUI stuff/BuilderHealth.cs
+++ Assets/GUI stuff/BuilderHealth.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoldierHealth : MonoBehaviour {
+public class BuilderHealth : MonoBehaviour {
 
-	public float max_health = 300f;
+
+	public float max_health = 100f;
 	public float cur_health = 0f;
-	public float attack_stat = 25f;
 	public GameObject HealthBar;
 	// Use this for initialization
 	void Start () {
 		cur_health = max_health;
-		InvokeRepeating("decreasehealth", 10f, 10f);
+		InvokeRepeating("decreasehealth", 1f, 1f);
 	}
 	
 	// Update is called once per frame
@@ -19,7 +19,7 @@ public class SoldierHealth : MonoBehaviour {
 		
 	}
 	void decreasehealth(){
-		cur_health -= 2;
+		cur_health -= 20;
 		float calc_health = cur_health / max_health;
 		setHealthbar(calc_health);
 		destroyOnDeath();
@@ -36,3 +36,5 @@ public class SoldierHealth : MonoBehaviour {
 		HealthBar.transform.localScale = new Vector3 (myHealth, HealthBar.transform.localScale.y, HealthBar.transform.localScale.z);
 	}
 }
+
+
